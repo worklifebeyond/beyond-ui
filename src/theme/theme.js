f458f1b225edb5a79845a19f757c0fadcead644d
positiveneutron <D10815802@mail.ntust.edu.tw>
2020-03-08 12:16:27 +0700

    add color pallete theme based on zeplin local Styleguide

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,22 +1,33 @@
 const theme = {
+  colorPallete: {
+    azure: `#039be5`,
+    black: `#000000`,
+    deepTeal: `#014a62`,
+    greyish: `#a9a8a8`,
+    midGreen: `#4caf50`,
+    orangeYellow: `#ffa000`,
+    paleGrey: `#f7f8f9`,
+    white: `#ffffff`,
+    waterMelon: `#ef4d5e`
+  },
   text: {
     color: `#fff`,
     fontFamily: `Muli`,
-    fontSize: `20px`,
+    fontSize: `20px`
   },
   button: {
     margin: {
       horizontal: `0.2em`,
-      vertical: `0.2em`,
+      vertical: `0.2em`
     },
     padding: {
       horizontal: `2em`,
-      vertical: `2em`,
+      vertical: `2em`
     },
     shadow: {
-      offset: `(0em / 2)`,
-    },
-  },
-}
+      offset: `(0em / 2)`
+    }
+  }
+};
 
-export default theme
+export default theme;
